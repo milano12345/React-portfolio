bd74ec548a0bd41a5e2f9201b0b78c370212cf95
milano12345 <dmilano857@gmail.com>
2019-07-24 00:15:18 -0500

    input all images into cards. need to fix css

--- my-app/src/Components/Projects.js
+++ my-app/src/Components/Projects.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Tabs, Tab, CardTitle, CardText, CardActions, Card, Button, Grid, Cell } from 'react-mdl';
-
+import Bakery from './images/01.jpg'
 class Projects extends Component {
     constructor(props) {
         super(props);
@@ -12,7 +12,7 @@ class Projects extends Component {
             return <Grid className="demo-grid-1">
             <Cell col={5}>
                     <Card className='card'>
-                    <CardTitle expand style={{ color: '#fff', background: 'url(https://codepen.io/Fall3n/pen/eXwNOO) bottom right 15% no-repeat #46B6AC' }}>Bakery Website</CardTitle>
+                    <CardTitle expand style={{ color: '#fff', background: 'url(https://milano12345.github.io/images/gallery/thumbs/01.jpg) bottom right 50% no-repeat' }}>Bakery Website</CardTitle>
                         <CardText>
                             This was made using HTML/CSS
                             </CardText>
@@ -23,7 +23,7 @@ class Projects extends Component {
                      </Cell>
                      <Cell col={5}>
                     <Card className='card'>
-                    <CardTitle expand style={{ color: '#fff', background: 'url(https://github.com/milano12345/responsive-web-design-I) bottom right 15% no-repeat #46B6AC' }}>Responsive Website</CardTitle>
+                    <CardTitle expand style={{ color: '#fff', background: 'url(https://milano12345.github.io/images/Screenshot%20(6).png) bottom right 50% no-repeat #46B6AC' }}>Responsive Website</CardTitle>
                         <CardText>
                             This was made using HTML/CSS. Integrating responsive web design.
                             </CardText>
@@ -34,7 +34,7 @@ class Projects extends Component {
                      </Cell>
                      <Cell col={5}>
                     <Card className='card'>
-                    <CardTitle expand style={{ color: '#fff', background: 'url(https://github.com/milano12345/Sprint-Challenge--Advanced-CSS/tree/daniel-milano) bottom right 15% no-repeat #46B6AC' }}>Advanced CSS challenge</CardTitle>
+                    <CardTitle expand style={{ color: '#fff', background: 'url(https://milano12345.github.io/images/Screenshot%20(7).png) bottom right 50% no-repeat #46B6AC' }}>Advanced CSS challenge</CardTitle>
                         <CardText>
                             This was made using HTML/CSS using advanced techniques.
                             </CardText>
@@ -45,7 +45,7 @@ class Projects extends Component {
                      </Cell>
                      <Cell col={5}>
                     <Card className='card'>
-                    <CardTitle expand style={{ color: '#fff', background: 'url(https://github.com/milano12345/Sprint-Challenge--User-Interface) bottom right 15% no-repeat #46B6AC' }}>User Interface Challenge</CardTitle>
+                    <CardTitle expand style={{ color: '#fff', background: 'url(https://milano12345.github.io/images/Screenshot%20(8).png) bottom right 50% no-repeat #46B6AC' }}>User Interface Challenge</CardTitle>
                         <CardText>
                             This was made using HTML/CSS. Integrating Flexbox.
                             </CardText>
@@ -56,7 +56,7 @@ class Projects extends Component {
                      </Cell>
                      <Cell col={5}>
                     <Card className='card'>
-                    <CardTitle expand style={{ color: '#fff', background: 'url(https://github.com/milano12345/Preprocessing-II) bottom right 15% no-repeat #46B6AC' }}>Preprocessor Use</CardTitle>
+                    <CardTitle expand style={{ color: '#fff', background: 'url(https://milano12345.github.io/images/Screenshot%20(9).png) bottom right 50% no-repeat #46B6AC' }}>Preprocessor Use</CardTitle>
                         <CardText>
                             This was made using HTML/CSS. Utitilizing the Preprocessor LESS.
                             </CardText>
