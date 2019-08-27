60ffe575f9c5ff9782c2e23c162135765ea4ac00
milano12345 <dmilano857@gmail.com>
2019-08-27 17:01:49 -0500

    added footer

---
+++ my-app/src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function BottomNav() {
+
+    return (
+        <div className='nav-wrapper'>
+        <p style={{textAlign:'center'}}>
+		&copy; Daniel Milano Design
+	</p>
+        </div>
+    )
+}
+
+export default BottomNav;
