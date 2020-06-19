ab8c2070f2846f512bbc5ba2d607c79576df670e
milano12345 <dmilano857@gmail.com>
2020-06-18 22:34:48 -0500

    updated landing photo + resume page

--- my-app/src/Components/Education.js
+++ my-app/src/Components/Education.js
@@ -1,22 +1,24 @@
-import React,{Component} from 'react';
-import {Grid, Cell} from 'react-mdl';
-
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Education extends Component {
-    render(){
-        return (
-            <Grid>
-                <Cell col={4}>
-                <h4>{this.props.startYear} - {this.props.endYear}</h4>
-                </Cell>
-                <Cell col={8}>
-                    <h4 style={{marginTop: '22px'}}>{this.props.schoolName}</h4>
-                    <p style={{color:'darkgray', fontSize: '16px'}}>{this.props.schoolDescription}</p>
-                </Cell>
-            </Grid>
-        )
-    }
-
+  render() {
+    return (
+      <Grid>
+        <Cell col={4}>
+          <h4>
+            {this.props.startYear} - {this.props.endYear}
+          </h4>
+        </Cell>
+        <Cell col={8}>
+          <h4 style={{ marginTop: "22px" }}>{this.props.Name}</h4>
+          <p style={{ color: "darkgray", fontSize: "16px" }}>
+            {this.props.Description}
+          </p>
+        </Cell>
+      </Grid>
+    );
+  }
 }
 
 export default Education;
--- my-app/src/Components/Resume.js
+++ my-app/src/Components/Resume.js
@@ -1,37 +1,78 @@
-import React, {Component} from 'react';
-import {Grid, Cell} from 'react-mdl';
-import Education from './Education'
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import Education from "./Education";
 
 class Resume extends Component {
-    render() {
-        return (
-            <div>
-                <Grid>
-                    <Cell className='resume-left-col' col={4}>
-                        <div style={{textAlign: 'center'}}>
-                    <img src={require("./images/IMG_1569.jpg")} className="avatar-img2" alt={'Computer Edited Pencil Drawing'} />
-                             </div>
-                        <h2>Daniel Milano</h2>
-                        <h3 style={{color: 'grey'}}>Programmer</h3>
-                        <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}></hr>
-                        <div className='text-card'>
-                        <p style={{textAlign: 'left', padding:'16px', color:'darkgray'}}>Daniel is a driven, motivated individual with a passion for learning new technologies. He has been tinkering with websites since the young age of 10. From running bots on Runescape to save precious time,  utilizing Tamper data on Firefox to hack games in the early stages of Facebook, rooting Android phones/ flashing ROMs, and of course, full design customization on MySpace. Daniel has since matured and now spends his time more productively, learning full stack web development so he can contribute something meaningful and worthwhile to mankind.</p>
-                        </div>
-                    </Cell>
-                    <Cell className='resume-right-col' col={8}>
-                        <h2 style={{marginLeft: '14px'}}>Education</h2>
-                        <Education 
-                        startYear={2019} 
-                        endYear={2020} 
-                        schoolName={'Lambda School'}
-                        schoolDescription='Lambda School is a 9+ month Computer Science & Software Engineering Academy that provides an immersive hands-on curriculum with a focus on computer science, and fullstack web development.
-                            -Completed all curriculum course work including: React, React Native, Redux, Node, Express, MongoDB, Jest, Python, Django, etc.
-                            -Write production-ready code using ReactJS, Redux, and CSS on the frontend and NodeJS and Express on the backend to build single page applications.' />
-                    </Cell>
-                </Grid>
+  render() {
+    return (
+      <div>
+        <Grid>
+          <Cell className="resume-left-col" col={4}>
+            <h2>Daniel Milano</h2>
+            <h3 style={{ color: "grey" }}>Programmer</h3>
+            <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }}></hr>
+            <div style={{ textAlign: "center" }}>
+              <img
+                src={"https://danielmilano.tech/images/milano3.jpg"}
+                className="avatar-img2"
+                alt={"Computer Edited Pencil Drawing"}
+              />
             </div>
-        )
-    }
+            <div className="text-card">
+              <h5
+                style={{
+                  color: "grey",
+                  paddingRight: "5%",
+                  paddingLeft: "5%",
+                  paddingTop: "5%",
+                }}
+              >
+                Summary
+              </h5>
+              <p
+                style={{
+                  textAlign: "left",
+                  padding: "16px",
+                  fontSize: "16px",
+                  color: "darkgray",
+                  marginBottom: "-3%",
+                }}
+              >
+                Daniel is a driven, motivated individual with a passion for
+                learning new technologies. He has been tinkering with websites
+                since he was 10 years old. From running bots on Runescape to
+                save precious time, rooting Android phones/ flashing ROMs, and
+                of course, full design customization on MySpace.
+              </p>
+              <p
+                style={{
+                  textAlign: "left",
+                  padding: "16px",
+                  fontSize: "16px",
+                  color: "darkgray",
+                }}
+              >
+                Daniel has since matured and now spends his time more
+                productively, learning full stack web development so he can
+                contribute something meaningful to mankind.
+              </p>
+            </div>
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            <h2 style={{ marginLeft: "14px" }}>Education</h2>
+            <Education
+              startYear={2019}
+              endYear={2020}
+              Name={"Lambda School"}
+              Description="Lambda School is a 9+ month Computer Science & Software Engineering Academy that provides an immersive hands-on curriculum with a focus on computer science, and fullstack web development.
+                            -Completed all curriculum course work including: React, React Native, Redux, Node, Express, MongoDB, Jest, Python, Django, etc.
+                            -Write production-ready code using ReactJS, Redux, and CSS on the frontend and NodeJS and Express on the backend to build single page applications."
+            />
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Resume;
